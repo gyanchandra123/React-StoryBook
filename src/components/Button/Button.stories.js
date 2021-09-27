04a6b24e0a1fd7ffb01106c3e8bc5ca1eb2ddb2e
gyanchandra moirangthem <gyanchandra moirangthem>
2021-09-27 06:49:44 +0530

    React Storybook Tutorial - 5 - Decorators: By using the decorator property && placing the stories at the center

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -7,7 +7,8 @@ export default {
   component: Button,// this is the component for which we need to create the story
   args: {
     children:'Button' // maintening the same children value for all the stories for the book component.
-  }
+  },
+  decorators:[(story) =><Center>{story()}</Center>]
 };   
 
 /* export const Primary = () => <Button variant='primary'>Primary</Button>
@@ -15,7 +16,7 @@ export const Secondary = () => <Button variant='secondary'>Secondary</Button>
 export const Success = () => <Button variant='success'>Success</Button>
 export const Danger = () => <Button variant='danger'>Danger</Button> */
                           
-const Template =  args => <Center> <Button {...args} /></Center>
+const Template =  args => <Button {...args} />
 
 export const PrimaryA = Template.bind({});
 PrimaryA.args = {
