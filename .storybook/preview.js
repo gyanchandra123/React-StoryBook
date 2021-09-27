ce22346830ac1a502d50cca853c9c729524fc5dd
gyanchandra moirangthem <gyanchandra moirangthem>
2021-09-27 09:15:13 +0530

    React Storybook Tutorial - 6.2 - Decorators and Theming in V6

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -18,7 +18,8 @@ export const parameters = {
 //global decorator:
 addDecorator((story) =><Center>{story()}</Center>) */
 
-import React from 'react';
+/* decorator and theming in SB version-5 */
+/* import React from 'react';
 import {addDecorator} from '@storybook/react';
 import { ThemeProvider, theme, CSSReset, Box } from '@chakra-ui/core';
 
@@ -38,4 +39,32 @@ addDecorator((story) => (
     {story()} 
     </Box>
   </ThemeProvider>
-))
+)) */
+
+
+/* decorator and theming in SB version-6 */
+import React from 'react';
+import {addDecorator} from '@storybook/react';
+import { ThemeProvider, theme, CSSReset, Box } from '@chakra-ui/core';
+
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  options: {
+    storySort: (a, b) =>
+      a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
+  }
+}
+
+export const decorators =[
+  (story) =>(
+    <ThemeProvider theme={theme}>
+    <CSSReset /> 
+    <Box m='4'> 
+    {story()} 
+    </Box>
+  </ThemeProvider>
+  )
+]
+
+ 
