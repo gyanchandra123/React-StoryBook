f14b66ed7d4c956945b36fa61c7f673b17829330
gyanchandra moirangthem <gyanchandra moirangthem>
2021-10-04 12:15:00 +0530

    React Storybook Tutorial - 3 - Writing Stories

---
+++ src/component-attempt3/Button/Button.js
@@ -0,0 +1,27 @@
+/* import React from "react";
+import "./Button.css";
+
+const Button = (props) => {
+  const { variant = "primary", children, ...rest } = props;
+
+  return (
+    <button className={`button${variant}`} {...rest}>
+      {children}
+    </button>
+  );
+};
+
+export default Button; */
+
+import React from "react";
+import './Button.css';
+
+function Button(props) {
+  const { variant = 'primary', children, ...rest } = props;
+  return (
+  <button className={`button ${variant}`} {...rest}>
+      {children}
+  </button>)
+}
+
+export default Button;
